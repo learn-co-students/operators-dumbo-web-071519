c14abeae867e01e513ca4e95295820eca0f1d993
Ashna Shah-Grover <as19@williams.edu>
2019-05-09 17:03:12 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
+require 'pry'
+
 def unsafe?(speed)
-
+  if (40..60).include?(speed)
+    return true
+  else 
+    return false 
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+	(40..60).include?(speed) ? true : false 
 end
+
+
 	
+
+
 
 
