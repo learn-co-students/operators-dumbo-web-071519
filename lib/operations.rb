17d628b0981e61736dbbdf92ed65f27639d696a2
Carlos Guzman <tomaffew@me.com>
2019-07-20 22:30:54 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
-
+  if (40..60).include?(speed)
+    return false
+  else 
+    return true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  if (40..60).include?(speed)
+    return false
+  else 
+    return true
+  end
 end
 	
 
